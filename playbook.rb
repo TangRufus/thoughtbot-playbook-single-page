6a6145d5489e129076ca124b169f27f830029ff4
TangRufus <tangRufus@gmail.com>
2012-09-25 06:07:52 +0800

    support for pulling the HTML5 version playbook

--- playbook.rb
+++ playbook.rb
@@ -9,19 +9,20 @@ doc = Nokogiri::HTML.parse(File.open('home.html', 'r:utf-8'))
 puts "Parsing table of contents"
 
 toc = []
-doc.css('section#chapters a').each do |a|
+doc.css('nav a').each do |a|
   text = a.inner_text
-  if a.parent.name == 'h2'
+  if a.parent.name == 'h2' 
     toc << {:section => text, :href => a[:href]}
-  elsif a.parent.name == 'p'
+  elsif a.parent.name == 'li'
     toc << {:page => text, :href => a[:href]}
   end
 end
 
-toc_doc = Nokogiri::HTML(File.read("home.html")).at("section#chapters")
+toc_doc = Nokogiri::HTML(File.read("home.html")).at("nav ul")
+toc_doc.xpath('//@style').remove
 toc_html = toc_doc.serialize.gsub(/ id=[^>]*/, '')
 
-puts "Loading pages"
+puts "Loading pages... This may take a while depending on your connection..."
 
 big_html = toc.inject([toc_html]) do |memo, x|
   puts x[:href]
@@ -66,8 +67,12 @@ doc.search('a').each do |a|
   href = a[:href]
   if href =~ /^\//
     print '.'
-    new = "<a href='##{href.sub('/', '').gsub(/\W/, '_')}'>#{a.inner_text}</a>"
+    new = "<a href='##{href.gsub(/-/, '_').split('/').last}'>#{a.inner_text}</a>"
     a.swap(new)
+  elsif href =~ /^./
+  print '.'
+  new = "<span>#{a.inner_text}</span>"
+  a.swap(new)
   end
 end
 puts 
